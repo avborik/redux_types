f3e7abaeaf6f1675fb54198051df024c45f37c01
AvBorikDeveloper <avboris28@gmail.com>
2019-08-02 01:46:58 +1200

    Using Types

---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const GET_MOVIES = 'GET_MOVIES';
+export const GET_GAMES = 'GET_GAMES';
+export const GET_CELEBRITIES = 'GET_CELEBRITIES';
--- src/reducers/data_reducer.js
+++ src/reducers/data_reducer.js
@@ -1,3 +1,7 @@
+import {
+    GET_MOVIES
+} from '../actions/types'
+
 export default function(state={}, action){
     switch(action.type){
         case 'GET_MOVIES':
